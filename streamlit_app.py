975a124b0ec2f866941eaf95ec5d5388b5f81b9c
Jochhan Banerjee <109904636+jochhanbanerjee07@users.noreply.github.com>
2022-07-25 00:52:47 +0530

    Update streamlit_app.py
    removed the response code output and also the denormalized json output

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,12 +27,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon") 
-streamlit.text(fruityvice_response) # Just writes data to the screen
 
 # takes response json content and normalizes it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-
-streamlit.text(fruityvice_response.json())
